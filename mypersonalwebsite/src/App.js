b47e1749f33a16f62c438bbc3c4e7302334251a2
BrianDCF99 <bda59@sfu.ca>
2023-11-20 00:24:50 -0800

    #8 wrote some html on the app.js and changed styling on app.css

--- mypersonalwebsite/src/App.js
+++ mypersonalwebsite/src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Assuming you have a CSS file for styling
+
+const name = {
+  name: 'Brian Del Carpio',
+  imagePath: '../images/Selfie.jpg',
+};
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="top-right-menu">
+          <button onClick={toggleMenu}>More</button>
+          {isMenuOpen && (
+            <div className="dropdown-menu">
+              {/* Dropdown menu content goes here */}
+              <ul>
+                <li>About</li>
+                <li>Current Projects</li>
+                <li>Work Experience</li>
+                <li>Education</li>
+                <li>Other Links</li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+
+        <img src={name.imagePath} alt="Brian's Picure" />
+        <h1>{name.name}</h1>
+        
       </header>
     </div>
   );
